623b723de4ee6b6e8177aaa9078873caf8aa551f
tuleys <sophie.tuley@nhs.net>
2024-09-17 10:47:54 +0100

    Revert "my_calc: added multiply function"
    This reverts commit c48c29f1ff084794c73faf09e49743ff84ebcd88.

--- my_calc.py
+++ my_calc.py
@@ -7,9 +7,3 @@ def subtract_numbers(num_1, num_2):
     result = num_1 - num_2
 
     return result
-
-def multiply_numbers(num_1, num_2):
-    while True:
-        result = num_1 * num_2
-
-    return result
