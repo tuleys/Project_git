c48c29f1ff084794c73faf09e49743ff84ebcd88
tuleys <sophie.tuley@nhs.net>
2024-09-17 10:36:48 +0100

    my_calc: added multiply function

--- my_calc.py
+++ my_calc.py
@@ -7,3 +7,9 @@ def subtract_numbers(num_1, num_2):
     result = num_1 - num_2
 
     return result
+
+def multiply_numbers(num_1, num_2):
+    while True:
+        result = num_1 * num_2
+
+    return result
